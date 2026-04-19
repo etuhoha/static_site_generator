4647e5d44d4abf3b7fb22ae56f71106e7c75b895
etuhoha <etuhoha@gmail.com>
2026-04-19 17:29:07 +0300

    publishing

--- src/main.py
+++ src/main.py
@@ -1,33 +1,34 @@
 import os
 import shutil
+import sys
 
 from htmlnode import extract_title, markdown_to_html_node
 from pathlib import Path
 
 def main():
+    basepath = "/"
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    print(f"basepath: {basepath}")
+
     src_dir = "static"
-    tgt_dir = "public"
+    tgt_dir = "docs"
     if os.path.exists(tgt_dir):
         shutil.rmtree(tgt_dir)
     copy_dir(src_dir, tgt_dir)
 
-    generate_pages_recursive("content", "template.html", "public")
-    # generate_page("content/index.md", "template.html", "public/index.html")
-    # generate_page("content/blog/glorfindel/index.md", "template.html", "public/blog/glorfindel/index.html")
-    # generate_page("content/blog/tom/index.md", "template.html", "public/blog/tom/index.html")
-    # generate_page("content/blog/majesty/index.md", "template.html", "public/blog/majesty/index.html")
-    # generate_page("content/contact/index.md", "template.html", "public/contact/index.html")
+    generate_pages_recursive(basepath, "content", "template.html", tgt_dir)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(basepath, dir_path_content, template_path, dest_dir_path):
     for f in os.listdir(dir_path_content):
         p = os.path.join(dir_path_content, f)
         if os.path.isfile(p):
             df = os.path.splitext(f)[0] + ".html"
-            generate_page(p, template_path, os.path.join(dest_dir_path, df))
+            generate_page(basepath, p, template_path, os.path.join(dest_dir_path, df))
         else:
-            generate_pages_recursive(p, template_path, os.path.join(dest_dir_path, f))
+            generate_pages_recursive(basepath, p, template_path, os.path.join(dest_dir_path, f))
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(basepath, from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 
     contents_string = ""
@@ -42,6 +43,7 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(contents_string)
 
     html = template_html.replace("{{ Title }}", title).replace("{{ Content }}", contents_html)
+    html = html.replace('href="/', f"href=\"{basepath}").replace('src="/', f"src=\"{basepath}")
     Path(os.path.dirname(dest_path)).mkdir(parents=True, exist_ok=True)
     with open(dest_path, 'w') as file:
         file.write(html)

a6fe7d65d8d1a87e1b40bc2e3867a96671aaf4f7
etuhoha <etuhoha@gmail.com>
2026-04-19 16:18:32 +0300

    copy static

--- src/main.py
+++ src/main.py
@@ -1,8 +1,28 @@
-from textnode import TextNode
+import os
+import shutil
 
+def copy_dir(src_dir, tgt_dir):
+    print(f"copying contents of dir: {src_dir} -> {tgt_dir}")
+    if not os.path.exists(tgt_dir):
+        os.mkdir(tgt_dir)
+
+    fs = os.listdir(src_dir)
+    for f in fs:
+        sf = os.path.join(src_dir, f)
+        tf = os.path.join(tgt_dir, f)
+        if os.path.isfile(sf):
+            print(f"copying file: {sf} -> {tf}")
+            shutil.copy(sf, tf)
+        else:
+            copy_dir(sf, tf)
 
 def main():
-    t = TextNode("This is some anchor text", "link", "https://www.boot.dev")
-    print(t)
+    src_dir = "./static"
+    tgt_dir = "./public"
+    if os.path.exists(tgt_dir):
+        shutil.rmtree(tgt_dir)
+    copy_dir(src_dir, tgt_dir)
+
+
 
 main()
